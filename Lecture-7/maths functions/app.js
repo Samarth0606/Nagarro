c6ca068658458002f450a3966ef391d5d4b78a71
Samarth Vohra <kartikmathur@Sohails-Macbook-Pro.local>
2022-06-27 18:38:25 +0530

    lecture 7 evening

---
+++ Lecture-7/maths functions/app.js
@@ -0,0 +1,162 @@
+// round 
+// Math.round(3.24)
+// 3
+// Math.round(3.44)
+// 3
+// Math.round(3.64)
+// 4
+// Math.round(3.5)
+// 4
+// Math.round(3.547)
+// 4
+// Math.round(-3.4)
+// -3
+// Math.round(-3.5)
+// -3
+// Math.round(-3.51)
+// -4
+// Math.round(3.5)
+// 4
+// Math.round(-3.500000)
+// -3
+
+
+// /random
+// 8
+// Math.floor((Math.random()*10))
+// 4
+// Math.floor((Math.random()*10))
+// 5
+// Math.floor((Math.random()*10))
+// 6
+// Math.floor((Math.random()*10))
+// 6
+// Math.floor((Math.random()*10))
+// 1
+// Math.floor((Math.random()*10))
+// 0
+// Math.floor((Math.random()*10))
+// 4
+// Math.floor((Math.random()*10))
+// 0
+// Math.floor((Math.random()*10))
+// 9
+// Math.floor((Math.random()*10))
+// 1
+// Math.floor((Math.random()*10))
+// 1
+// Math.floor((Math.random()*10))
+// 1
+
+// floor & ceil
+// Math.floor(3.4)
+// 3
+// Math.floor(3.8)
+// 3
+// Math.floor(3.9999999999999009090i68)
+// VM1667:1 Uncaught SyntaxError: Invalid or unexpected token
+// Math.floor(3.999999999999900909068)
+// 3
+// Math.ceil(3.999999999999900909068)
+// 4
+// Math.ceil(3.0000001)
+// 4
+// Math.ceil(3.5000001)
+// 4
+
+// pi
+// Math.PI
+// 3.141592653589793
+// let ans = 2*Math.PI*4
+// undefined
+// ans
+// 25.132741228718345
+
+// abs
+// let ans= 10;
+// undefined
+// Math.abs(ans)
+// 10
+// Math.abs(-ans)
+// 10
+// Math.abs(-102102812091921)
+// 102102812091921
+// Math.abs(102102812091921)
+// 102102812091921
+
+// /sqrt
+// let square = 36;
+// undefined
+// Math.sqrt(square)
+// 6
+// Math.sqrt(35)
+// 5.916079783099616
+// Math.sqrt(37)
+// 6.082762530298219
+
+// generating random numbers bt 2 givenno 22-25
+// Math.floor(Math.random()*4)+22;
+// 24
+//     Math.floor(Math.random()*4)+22;
+// 23
+//     Math.floor(Math.random()*4)+22;
+// 25
+//     Math.floor(Math.random()*4)+22;
+// 23
+//     Math.floor(Math.random()*4)+22;
+// 23
+//     Math.floor(Math.random()*4)+22;
+// 24
+//     Math.floor(Math.random()*4)+22;
+// 22
+//     Math.floor(Math.random()*4)+22;
+// 25
+//     Math.floor(Math.random()*4)+22;
+// 23
+//     Math.floor(Math.random()*4)+22;
+// 23
+//     Math.floor(Math.random()*4)+22;
+// 23
+//     Math.floor(Math.random()*4)+22;
+
+
+
+// taking user's input
+
+
+// let age = prompt(" enter your age");
+
+// if(age<18){
+//     console.log("you are not alowed to enter the club");
+// }
+
+// else if(age>=18 && age<25){
+//     console.log("you can enter but you cannot drink");
+// }
+
+// else{
+//     console.log("welcome to the club , you r now your responsibilty");
+// }
+
+
+// arrays
+
+// let arr = [1,2,3,"sam","nagarro","aditya sir",undefined, null , [5,6,7, [8,9,0]]]
+
+
+
+
+
+           
+// undefined
+// arr
+// (9) [1, 2, 3, 'sam', 'nagarro', 'aditya sir', undefined, null, Array(4)]0: 11: 22: 33: "sam"4: "nagarro"5: "aditya sir"6: undefined7: null8: (4) [5, 6, 7, Array(3)]length: 9[[Prototype]]: Array(0)
+// arr[9]
+// undefined
+// arr[8]
+// (4) [5, 6, 7, Array(3)]0: 51: 62: 73: (3) [8, 9, 0]length: 4[[Prototype]]: Array(0)
+// arr[8][3][2]
+// 0
+
+
+
