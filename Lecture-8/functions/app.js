707d4c0fcffa4c98bb79398c22aea5f936700335
Samarth Vohra <kartikmathur@Sohails-Macbook-Pro.local>
2022-06-28 18:33:45 +0530

    lecture 8 evening

---
+++ Lecture-8/functions/app.js
@@ -0,0 +1,82 @@
+// function defination
+// function dog(){
+//     console.log("bark");
+//     for(let i=1;i<5;i++){
+//         console.log(i);
+//     }
+//     console.log("dog is happy");
+
+// }
+
+
+// function calling
+// dog();
+
+// parameterised function
+
+// function addition(a,b,c){
+//     let ans  = a+b+c;
+//     if(ans<0){
+//         return;
+//     }
+//     console.log(ans);
+// }
+
+// addition(2,3); // nan
+
+
+// funiton expression
+// let call = function (name){
+//     if(name == "sam"){
+//         console.log("welcome abhi");
+//     }
+//     else{
+//         console.log("plz enter  vlaid user");
+//     }
+// }
+
+
+// call("samm");
+
+
+// /anonymous fucntion
+// let anony = function (name){
+//     if(name == "sam"){
+//         // console.log("welcome samarth");
+//         console.log("anonymous function");
+//     }
+//     else{
+//         console.log("wrong technique");
+//     }
+// }
+
+
+// call("samm");
+
+
+// function add(a,b){
+//     let ans = a+b;
+//     console.log(ans);
+// }
+
+// questions
+
+// 1.
+// add(true , 10);
+// 2.
+// add(true , "samarth");
+// 3.
+// add(10,"vishesh")
+
+
+// let sam = bool;
+// console.log(sam);
+// typecastting eg
+// [] == 0
+// "" == 0
+// [] == ""
+
+
+
+
+
